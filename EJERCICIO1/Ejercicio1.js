baeb5f2466ebe57d351fcf11be7028a3b641a3d4
Cons83 <112658821+Cons83@users.noreply.github.com>
2022-09-01 17:55:20 -0300

    Add files via upload
    Exercises 1 & 2

---
+++ EJERCICIO1/Ejercicio1.js
@@ -0,0 +1,7 @@
+let nombre="maria"
+let apellido="Losada"
+let direccion="123456"
+console.log("Mi nombre es "+nombre+",mi apellido "+apellido+" y mi direccion "+direccion)
+console.log (dni)
+
+console.log("mi nombre es")
---
+++ EJERCICIO2/Ejercicio2.js
@@ -0,0 +1,6 @@
+let numeroUno= 7
+let numeroDos= 4
+let suma= numeroUno+numeroDos
+console.log("El numero uno es "+numeroUno)
+console.log("El numero dos es "+numeroDos)
+console.log(suma)
