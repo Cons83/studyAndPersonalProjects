2532564c8f5901129604e75541af5bd16c03cd0b
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-09-02 16:15:38 -0300

    Add files via upload

---
+++ EJERCICIO3/Ejercicio3.js
@@ -0,0 +1,6 @@
+let segundos=32400
+let minutos=segundos/60
+let horas=(minutos/60)
+console.log(segundos)
+console.log(minutos+" minutos")
+console.log(horas+" horas")
---
+++ EJERCICIO4/Ejercicio4.js
@@ -0,0 +1,13 @@
+segundaFuncion()//llamar funcion; se respeta orden 
+primerFuncion()
+primerFuncion()
+function primerFuncion()//programacion funcion; no se respeta orden 
+{
+    console.log("Hola, soy una funcion")//ejecucion funcion
+    
+}
+function segundaFuncion()
+{
+    console.log("Y a mi que me importa")
+}
+
