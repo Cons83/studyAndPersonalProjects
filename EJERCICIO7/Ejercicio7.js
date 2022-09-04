ad717a33ea0fef1cd589f795e7d81e1b7d5b7931
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-09-04 15:57:37 -0300

    Add files via upload

---
+++ EJERCICIO7/Ejercicio7.js
@@ -0,0 +1,11 @@
+//for(primer espacio:inicializacion;condicion;incremento)
+for(i=0;i<10;i++)// repite bucle 10 veces, desde 0, hasta 9
+{
+
+}
+let inicio=7
+let fin=100
+for(i=inicio;i<fin;i++)
+{console.log(i)
+
+}
---
+++ EJERCICIO8/Ejercicio8.js
@@ -0,0 +1,17 @@
+//condicionar info o evaluarla
+let edad=18
+if(edad<18)
+{
+console.log("Se trata de un menor")
+}else
+{
+ console.log("Es un mayor de edad")   
+}
+if(edad>70)
+{
+    console.log("Es una persona mayor")
+}
+if(edad<10)
+{
+    console.log("Es un infante")
+}
