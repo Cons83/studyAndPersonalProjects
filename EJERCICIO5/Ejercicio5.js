11ce796193ea86a88a7be9fbdbd3c96ac57b9aed
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-09-03 21:56:55 -0300

    Exercises 5 &6
    Exercises 5 & 6 javascript

---
+++ EJERCICIO5/Ejercicio5.js
@@ -0,0 +1,12 @@
+funcionImprimir(500)//llamado a la funcion con el envio de un parametro 
+funcionDividirnumero(183)
+
+function funcionImprimir(numero) // variable
+{
+    console.log(numero) //se imprime nro
+}
+
+function funcionDividirnumero(numero)
+{
+    console.log(numero/2)
+}
---
+++ EJERCICIO6/Ejercicio6.js
@@ -0,0 +1,6 @@
+let arreglo= ["T",4,5,"d",67,100,"Maria"]
+let largo= arreglo.length// calcula largo del arreglo con info interna;operador punto
+console.log(arreglo[2])
+console.log(arreglo[3])
+console.log(arreglo)
+console.log(largo)
