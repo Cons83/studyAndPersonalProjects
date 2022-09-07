44fe3e83f5dd8d39e7894c33c1bbc100f23eb2cd
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-09-07 17:30:31 -0300

    Add files via upload

---
+++ EJERCICIO15/main.js
@@ -0,0 +1,14 @@
+alert("Bienvenid@!!")
+let num = 1
+while (num > 0) {
+    num = parseInt(prompt("Ingrese numero"))
+    if (num % 2 == 0) {
+        alert("PAR")
+    }
+    else
+    {      alert("IMPAR")
+  }
+
+}
+alert("FIN DE PROGRAMA")
+alert("Gracias por usar nuestro software")
---
+++ EJERCICIO16/main.js
@@ -0,0 +1,8 @@
+let tipoDeAnimales= prompt ("Ingrese tipo de animal")
+while (tipoDeAnimales!="fin"){// el while empieza en las llaves
+    tipoDeAnimales=prompt ("Ingrese tipo de animal")
+}
+if (tipoDeAnimales == "fin"){
+    alert ("Gracias por usar nuestro software")
+}
+
