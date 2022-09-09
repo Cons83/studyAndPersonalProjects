af8c4369447aa851c7b03d0267f8c6daa0e00f0c
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-09-08 23:07:26 -0300

    Add files via upload

---
+++ EJERCICIO13/ejercicio13.js
@@ -0,0 +1,7 @@
+let num=30
+while(num <=126){
+if(!( num %2==0)){
+console.log(num)  
+    num+=1
+}
+}
---
+++ EJERCICIO14/ejercicio14.js
@@ -0,0 +1,7 @@
+let num = 0
+while (num <= 150){
+    if (!(num % 6==0)) {
+    console.log(num)
+        num += 1
+    }
+}
