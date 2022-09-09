1f6eb8f5008e5a150ddfcf0053623ec0d947e1b7
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-09-09 09:56:49 -0300

    Add files via upload

---
+++ EJERCICIO17/main.js
@@ -0,0 +1,7 @@
+//Hacer un programa que realice un Loop con while y que muestre los números 
+//comprendidos entre 10 y 1 de manera Decremental.
+let nro =10
+while (nro >=1){
+    console.log(nro)
+    nro-=1
+}
---
+++ EJERCICIO18/main.js
@@ -0,0 +1,7 @@
+let nro=1
+let acumulador=0
+while (nro<=6){
+ acumulador+=nro  //acumulador
+    nro++
+    console.log(acumulador)
+}
