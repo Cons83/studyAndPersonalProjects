70f854a01e6f1438c1ede7f2af2d98676a74645e
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-09-11 21:56:34 -0300

    Add files via upload

---
+++ EJERCICIO21/main.js
@@ -0,0 +1,7 @@
+let nro=1
+let paqueteDeGalletas= "Una galleta"
+while ( nro<=5){
+    paqueteDeGalletas=paqueteDeGalletas+ " otra galleta."
+nro++
+console.log(paqueteDeGalletas)
+}
---
+++ EJERCICIO22/main.js
@@ -0,0 +1,7 @@
+let nro=1
+let acumulador= "a"
+while (nro<=10){
+    acumulador= acumulador + "o"
+    nro++
+ console.log(acumulador)   
+}
