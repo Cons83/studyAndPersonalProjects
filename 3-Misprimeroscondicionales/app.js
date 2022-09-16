2d8747ffe0ab09b9908b2bf08a299d8f50ea5a61
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-09-16 16:18:56 -0300

    Add files via upload

---
+++ 3-Misprimeroscondicionales/app.js
@@ -0,0 +1,16 @@
+if(false){
+    console.log("Estoy dentro")
+}
+let estadoDeAnimo= prompt ("Como estas?")
+if (estadoDeAnimo=="triste"){
+console.log("No te pongas mal,a todos nos pasa.")
+}
+let claveSecreta= prompt ("Ingrese la clave secreta.")
+if (claveSecreta!=56){
+alert("Clave incorrecta.")
+}
+else {
+window.location = "http://www.google.com"
+}
+
+
---
+++ 4- Paroimpar/app.js
@@ -0,0 +1,6 @@
+let numero= prompt ("Ingrese un número")
+if (numero%2==0){
+alert ("Este numero es par.")
+}else {
+alert ("Este numero es impar.")
+}
