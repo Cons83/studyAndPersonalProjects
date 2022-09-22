7687aac18cf226457f70f9cbd89456db12b65973
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-09-22 19:55:31 -0300

    Add files via upload

---
+++ 1-QueImprimenEstosLoops/main.js
@@ -0,0 +1,25 @@
+let num = 0 //da inicio del loop (no hace falta poner la primera parte del while)
+while (num <= 100) {//mientras que
+    if (num % 4 == 0) {
+        console.log(num)
+        // suma de dos en dos
+    }
+    num += 1
+}
+console.log("EL otro while.")
+let num2 = 1
+while (num2 <= 20) {
+    if (!(num2 % 4 == 0)) {
+        console.log(num2)
+    }
+    num2++
+}
+
+
+
+
+
+
+
+
+
