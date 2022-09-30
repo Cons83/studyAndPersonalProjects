97466308580b7d28450e4b71e6ff158d9bc533fa
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-09-29 23:53:25 -0300

    Add files via upload

---
+++ 1-quedevuelvecadafuncion/main.js
@@ -0,0 +1,15 @@
+/*let resultado=0
+function test1(x, y) {
+    return y - x
+  }
+  
+  resultado=test1(10, 40)
+  console.log(resultado)*/
+
+  function test2(x) {
+    return x * 2//salis de la funcion
+    console.log(x)
+    return x / 2// en gris corte por return
+  }
+  
+  test2(10)//parametro transferida a la x
