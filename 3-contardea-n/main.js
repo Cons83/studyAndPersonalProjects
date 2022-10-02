0048bb6045c3585439733ce38aec76957bf382ee
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-10-01 23:52:25 -0300

    Add files via upload

---
+++ 3-contardea-n/main.js
@@ -0,0 +1,11 @@
+/*En este ejercicio deberás escribir una Función llamada contarDeA_n que tenga los Parámetros contar_de_a y contar_hasta.
+ Además, deberá escribir en la consola los números desde el 1 hasta contar_hasta en intervalos de contar_de_a.*/
+
+ function contarDeA_n (contar_de_a,contar_hasta){
+ let contador=0
+ while (contador<contar_hasta){
+    console.log(contador);
+    contador+=contar_de_a
+ }   
+
+ }
