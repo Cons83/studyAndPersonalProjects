2dc872eed7dcbd5aa668494b91f02845f454426f
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-10-03 00:01:06 -0300

    Add files via upload

---
+++ 5-Fechayhora/main.js
@@ -0,0 +1,7 @@
+/*En la consola de tu navegador, declará una Función que se llame fechaYHora. El bloque de código que
+ deberá ejecutar es un mensaje por consola que muestre la fecha y hora de este momento. Por ahora, escribilo manualmente.
+Invocá tu Función varias veces desde la consola.*/
+function fechaYhora(){
+console.log("La fecha y hora en este momento es: "+ Date())
+}
+console.log(fechaYhora())
