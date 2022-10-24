8f4318d42298e6a7e931c85c78c24d49e15db931
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-10-24 08:59:41 -0300

    Add files via upload

---
+++ 1-amigos/main.js
@@ -0,0 +1,6 @@
+let grupoDeAmigos =[
+    ["Harry", "Ron", "Hermione"],
+    ["Spiderman", "Hulk", "Ironman"],
+    ["Penélope Glamour", "Pierre Nodoyuna","Patán"]
+    ]
+    console.log(grupoDeAmigos[2][0])
