2040ecd7b622829db91ae67c7d86ff5485e60bb1
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-11-03 22:08:59 -0300

    Add files via upload

---
+++ 9-join/main.js
@@ -0,0 +1,3 @@
+let arr = ["Hola", "Mery", "¿cómo", "estas?"]
+arr.join(" ") // Le pasamos como Argumento un espacio.
+"Hola Mery ¿cómo estas?" // Nos devuelve un String con los datos separados por espacios.
