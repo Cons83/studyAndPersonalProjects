89f0c415ce527c448f2914969f3b977c01b1facc
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-11-08 23:58:56 -0300

    Add files via upload

---
+++ 11-iterar/main.js
@@ -0,0 +1,7 @@
+let colores=["Rojo", "Azul", "Verde", "Amarillo"] // Inicializo la Variable colores.
+let i = 0 // Inicializo la Variable Contador.
+
+while(i < colores.length){ // Se establece la condición.
+console.log(colores[i]) // Bloque de código que indica la acción que se ejecutará.
+i++ // Incremento del Contador que asegura la condición de corte.
+}
