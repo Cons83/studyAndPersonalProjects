9b574dae8c1975a5b4c4f8c18be8d7e5c3245f09
María Constanza Losada <112658821+Cons83@users.noreply.github.com>
2022-11-09 23:57:20 -0300

    Add files via upload

---
+++ 1-obj/main.js
@@ -0,0 +1,9 @@
+let auto = {
+    marca: "Tesla",
+    modelo: "Model Y",
+    kilometros: 0,
+    nuevo: true,
+    arrancar: function() {
+            alert("el auto se enciende!")
+        }
+    }
